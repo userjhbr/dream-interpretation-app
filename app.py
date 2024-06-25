16ea4faa02cf6bd1b3135e567124236e8afbfd93
userjhbr <126480143+userjhbr@users.noreply.github.com>
2024-06-25 14:51:21 -0300

    Update app.py
    modifica a função interpret_freud_and_jung para incluir a interpretação adequada nos dois casos: com e sem sentimentos

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ def interpret_dream_with_feelings():
     return jsonify({'interpretation': interpretation})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5002)
